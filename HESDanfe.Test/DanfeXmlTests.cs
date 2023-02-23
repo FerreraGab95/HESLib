91bd3b7fd7af9555eace15ae9c7629c89fef1bc4
Zonaro <zonaro@outlook.com>
2023-02-23 10:51:06 -0300

    -

--- HESDanfe.Test/DanfeXmlTests.cs
+++ HESDanfe.Test/DanfeXmlTests.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 using HESDanfe.Modelo;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -60,7 +61,7 @@ namespace HESDanfe.Test
             {
                 foreach (var pdf in d.Gerar(new DirectoryInfo(@"C:\Teste\testeDANFE")))
                 {
-
+                    Debug.WriteLine(pdf, "PDF Gerado");
 
                 }
 
